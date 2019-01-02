ac20e48ec0b1b7866fe50c7c7c3a95ef26aab4c0
Safuh <Safoh95@windowslive.com>
2019-01-01 23:39:55 -0800

    user can log in sign up  and log out

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,7 +5,7 @@ class MainPage extends React.Component {
     return (
       <div>
         <h1>Enspire</h1>
-        <footer>Copyright &copy; 2019 Chirper</footer>
+        <footer>Copyright &copy; 2019 Enspire</footer>
       </div>
     );
   }
