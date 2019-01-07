5efe5f29bd1c7e3d75875c0b283698336c4af7f1
jayad25 <dammalapati.jaya@gmail.com>
2019-01-07 09:47:55 -0800

    add barchart for performance show

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,7 +6,7 @@ import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
-
+import performance from './peroformace'
 import './reset.css'
 
 import Speech from './speech/speech_container'
@@ -14,6 +14,7 @@ import Speech from './speech/speech_container'
 
 const App = () => (
   <div>
+    <performance />
     <NavBarContainer />
     <AuthRoute path="/" component={MainPage} />
     <Switch>
