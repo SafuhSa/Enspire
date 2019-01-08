f9803e278ec755d25a18ac3c6eb09c67d4ef06ee
Safuh <Safoh95@windowslive.com>
2019-01-08 13:41:04 -0800

    topics

--- frontend/src/components/grammer/grammar_form.js
+++ frontend/src/components/grammer/grammar_form.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import "./grammar.css"
+import conversation from './conversation';
+import interview from './interview';
 
 class GrammarForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: "", prevs: "", idvView: '', stream: false, increment: 0 };
+    this.state = {
+      text: "",
+      prevs: "",
+      idvView: "",
+      stream: false,
+      prompt: ""
+    };
 
+    this.topic = 'Conversation'
+    this.prompt = conversation;
 
     this.transcript = "";
     this.handleSpeech = this.handleSpeech.bind(this);
 
     this.speaker = new SpeechSynthesisUtterance();
-    this.speaker.lang = 'en-US';
-    this.speaker.text = 'Welcome To Enspire';
+    this.speaker.lang = "en-US";
+    this.speaker.text = "Welcome To Enspire";
     speechSynthesis.speak(this.speaker);
-
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.renderErrors = this.renderErrors.bind(this);
@@ -23,161 +32,156 @@ class GrammarForm extends React.Component {
     this.renderLastCorrect = this.renderLastCorrect.bind(this);
     this.handlePormpt = this.handlePormpt.bind(this);
   }
-  
+
   handleSpeech(e) {
     e.preventDefault();
     let SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-    
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
     if (this.state.stream) {
-      
       this.setState({ stream: false });
       this.recognition.stop();
       this.recognition.removeEventListener("end", this.recognition.start);
       this.recognition = null;
-      
-      this.setState({text: this.transcript })
-      
+
+      this.setState({ text: this.transcript });
     } else {
-      
       this.setState({ stream: true });
-      
+
       this.recognition = new SpeechRecognition();
       this.recognition.interimResults = true;
-      
+
       const texts = document.querySelector(".text");
-      
+
       let p = document.createElement("p");
       texts.appendChild(p);
-      
+
       this.recognition.addEventListener("result", e => {
-        
         const transcript = Array.from(e.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join("");
-        
+          .map(result => result[0])
+          .map(result => result.transcript)
+          .join("");
+
         p.textContent = transcript;
         if (e.results[0].isFinal) {
           this.transcript += p.textContent + ". ";
-          this.setState({ text: this.transcript })
+          this.setState({ text: this.transcript });
           p = document.createElement("p");
           texts.appendChild(p);
         }
       });
-      
+
       this.recognition.start();
       this.recognition.addEventListener("end", this.recognition.start);
     }
   }
-  
-  
+
   componentWillMount() {
     this.props.fetchCorrections();
   }
-  
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.allCorrections) {
       this.setState({ prevs: Object.values(nextProps.allCorrections) });
     }
   }
-  
+
   updatetext(e) {
     this.setState({
       text: e.currentTarget.value
     });
   }
-  
+
   handleSubmit(e) {
     e.preventDefault();
-    
+
     this.props.correct(this.state.text);
   }
-  
-  
+
   renderLastCorrect() {
     let responses = this.props.lastCorrection;
     if (!responses) return null;
     let result = [];
-    
+
     for (let i = 0; i < responses.correcttext.length; i++) {
       const errs = responses.correcttext[i];
       const bad = errs.bad;
       const type = errs.type;
       const better = errs.better.slice(0, 2).join(" , ");
-      
+
       result.push(
         <div key={i}>
-         <ul>
-          <li>Err N# {i + 1}</li>
-          <li>bad: {bad} </li>
-          <li>better: {better} </li>
-          <li>type: {type} </li>
-          ------------------------------------
-        </ul>
+          <ul>
+            <li>Err N# {i + 1}</li>
+            <li>bad: {bad} </li>
+            <li>better: {better} </li>
+            <li>type: {type} </li>
+            ------------------------------------
+          </ul>
         </div>
       );
     }
     return result;
   }
-  
-  
-  
+
   speak(text) {
-    this.speaker.text = text
+    this.speaker.text = text;
     speechSynthesis.speak(this.speaker);
   }
 
   handlePormpt() {
-    // let file = require('./interview.txt')
-    // var readline = require('readline');
-    // var fs = require('fs');
-    // // debugger
-    // var myInterface = readline.createInterface({
-    //   input: fs.createReadStream("./interview.txt")
-    // });
-
-    // require("fs")
-      // .readFileSync("interview.txt")
-      // .toString()
-      // .split("\n");
-      // debugger
-
-    const arr = [ "Hi! How are you?", "What have you been up to today?", "Tell me about your family.", "What are your favorite foods?", "What would you like on your pizza?", "What is the weather like today?" ]
-
-    
+    let idx = (this.prompt.indexOf(this.state.prompt) + 1) % this.prompt.length;
+    let text = this.prompt[idx];
+    this.setState({ prompt: text });
   }
-  
+
+  changeTopic() {
+    if (this.prompt[2] === interview[2]) {
+      this.prompt = conversation
+      this.topic = "Conversation"
+      this.speak("Topic changed to Conversation");
+    } else {
+      this.prompt = interview
+      this.topic = "Interview";
+      this.speak("Topic changed to Interview");
+    }
+    this.setState({prompt: ''})
+  }
+
   renderErrors() {
     return (
       <ul>
         {Object.keys(this.props.errors).map((error, i) => (
           <li key={`error-${i}`}>{this.props.errors[error]}</li>
-          ))}
+        ))}
       </ul>
     );
   }
-  
+
   render() {
     let numErros;
     if (this.props.lastCorrection) {
       numErros = this.props.lastCorrection.correcttext.length;
     }
-    
-    
+
     let buttonText = this.state.stream ? "Stop" : "Record";
-    
-    
-    return <div className="grammar-page">
+
+    return (
+      <div className="grammar-page">
         <div>
           <div className="floater">
-            <button className="record-button">Change Topic</button>
-            <h1>Topic: Interview</h1>
+            <button className="record-button" onClick={this.changeTopic.bind(this)}>
+              Change Topic
+            </button>
+            <h1>{this.topic}</h1>
           </div>
 
           <div className="floater">
-            <button className="record-button" onClick={this.handlePormpt}>New Prompt</button>
-            <h2>{}</h2>
+            <button className="record-button" onClick={this.handlePormpt.bind(this)}>
+              New Prompt
+            </button>
+            {this.speak(this.state.prompt)}
+            <h2>{this.state.prompt}</h2>
           </div>
 
           <div className="text" />
@@ -195,13 +199,14 @@ class GrammarForm extends React.Component {
             </div>
             <input type="submit" value="Check Grammar" />
           </form>
-            <button onClick={() => this.speak(this.state.text)}>Read</button>
+          <button onClick={() => this.speak(this.state.text)}>Read</button>
           <div>
             <h3> Number of errors: {numErros} </h3>
             {this.renderLastCorrect()}
           </div>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 
